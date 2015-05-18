9d581b484131168cdcf332def248cba0b68125de
Alex Corbi <a.corbi@gmail.com>
2015-05-18 18:36:02 +0200

    Fixed some indentation

--- ckanext/odm_library/plugin.py
+++ ckanext/odm_library/plugin.py
@@ -24,71 +24,70 @@ def library_fields():
   return odm_library_helper.library_fields
 
 class OdmLibraryPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
-    '''OD Mekong library plugin.'''
+  '''OD Mekong library plugin.'''
 
-    plugins.implements(plugins.IDatasetForm)
-    plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.ITemplateHelpers)
-    plugins.implements(plugins.IRoutes, inherit=True)
-    plugins.implements(plugins.IFacets)
+  plugins.implements(plugins.IDatasetForm)
+  plugins.implements(plugins.IConfigurer)
+  plugins.implements(plugins.ITemplateHelpers)
+  plugins.implements(plugins.IRoutes, inherit=True)
+  plugins.implements(plugins.IFacets)
 
-    def dataset_facets(self, facets_dict, package_type):
+  def dataset_facets(self, facets_dict, package_type):
 
-      facets_dict = {
-                'license_id': toolkit._('License'),
-                'organization': toolkit._('Organizations'),
-                'groups': toolkit._('Groups'),
-                'tags': toolkit._('Tags'),
-                'res_format': toolkit._('Formats'),
-                'odm_language': toolkit._('Language'),
-                'odm_spatial_range': toolkit._('Country')
-                }
+    facets_dict = {
+              'license_id': toolkit._('License'),
+              'organization': toolkit._('Organizations'),
+              'groups': toolkit._('Groups'),
+              'tags': toolkit._('Tags'),
+              'res_format': toolkit._('Formats'),
+              'odm_language': toolkit._('Language'),
+              'odm_spatial_range': toolkit._('Country')
+              }
 
-      return facets_dict
+    return facets_dict
 
-    def group_facets(self, facets_dict, group_type, package_type):
+  def group_facets(self, facets_dict, group_type, package_type):
 
-      group_facets = {
-                'license_id': toolkit._('License'),
-                'organization': toolkit._('Organizations'),
-                'tags': toolkit._('Tags'),
-                'res_format': toolkit._('Formats'),
-                'odm_language': toolkit._('Language'),
-                'odm_spatial_range': toolkit._('Country')
-                }
+    group_facets = {
+              'license_id': toolkit._('License'),
+              'organization': toolkit._('Organizations'),
+              'tags': toolkit._('Tags'),
+              'res_format': toolkit._('Formats'),
+              'odm_language': toolkit._('Language'),
+              'odm_spatial_range': toolkit._('Country')
+              }
 
-      return group_facets
+    return group_facets
 
-    def organization_facets(self, facets_dict, organization_type, package_type):
+  def organization_facets(self, facets_dict, organization_type, package_type):
 
-      organization_facets = {
-                'license_id': toolkit._('License'),
-                'groups': toolkit._('Groups'),
-                'tags': toolkit._('Tags'),
-                'res_format': toolkit._('Formats'),
-                'odm_language': toolkit._('Language'),
-                'odm_spatial_range': toolkit._('Country')
-                }
+    organization_facets = {
+              'license_id': toolkit._('License'),
+              'groups': toolkit._('Groups'),
+              'tags': toolkit._('Tags'),
+              'res_format': toolkit._('Formats'),
+              'odm_language': toolkit._('Language'),
+              'odm_spatial_range': toolkit._('Country')
+              }
 
-      return organization_facets
+    return organization_facets
 
-    def before_map(self, m):
+  def before_map(self, m):
 
-      m.connect('library', #name of path route
-        '/library', #url to map path to
-        controller='ckanext.odm_library.controller:ThemeController',action='library')
+    m.connect('library', #name of path route
+      '/library', #url to map path to
+      controller='ckanext.odm_library.controller:ThemeController',action='library')
 
-      return m
+    return m
 
-    def update_config(self, config):
+  def update_config(self, config):
+    '''Update plugin config'''
 
-      '''Update plugin config'''
+    toolkit.add_template_directory(config, 'templates')
+    toolkit.add_resource('fanstatic', 'odm_library')
+    toolkit.add_public_directory(config, 'public')
 
-      toolkit.add_template_directory(config, 'templates')
-      toolkit.add_resource('fanstatic', 'odm_library')
-      toolkit.add_public_directory(config, 'public')
-
-    def get_helpers(self):
+  def get_helpers(self):
     '''Register the plugin's functions above as a template helper function.'''
 
     return {
@@ -98,7 +97,7 @@ class OdmLibraryPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
       'odm_library_is_library_orga': is_library_orga
     }
 
-    def _modify_package_schema_write(self, schema):
+  def _modify_package_schema_write(self, schema):
 
     for library_field in odm_library_helper.library_fields:
       validators_and_converters = [toolkit.get_validator('ignore_missing'),toolkit.get_converter('convert_to_extras'), ]
@@ -112,7 +111,7 @@ class OdmLibraryPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     return schema
 
   def _modify_package_schema_read(self, schema):
-
+    
     for library_field in odm_library_helper.library_fields:
       validators_and_converters = [toolkit.get_converter('convert_from_extras'),toolkit.get_validator('ignore_missing')]
       if library_field[2]:
