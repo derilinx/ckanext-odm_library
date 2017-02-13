2b10c96bd9e56d2627a690bbbf185a653925d7b1
Alex Corbi <a.corbi@gmail.com>
2017-02-13 18:23:35 +0100

    Fixes #58

--- ckanext/odm_library/lib/odm_library_helper.py
+++ ckanext/odm_library/lib/odm_library_helper.py
@@ -4,6 +4,7 @@
 import pylons
 import logging
 import json
+import os
 import logging
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
@@ -37,3 +38,32 @@ def create_default_issue_library_record(pkg_info):
     log.error("Action 'issue_create' not found. Please make sure that ckanext-issues plugin is installed.")
 
 session = {}
+
+def check_required_fields(package):
+	'''Checks that the package has all required fields'''
+
+	if DEBUG:
+		log.info('check_required_fields: %s', package)
+
+	valid = True
+
+	schema_path = os.path.abspath(os.path.join(__file__, '../../','odm_library_schema.json'))
+	with open(schema_path) as f:
+		try:
+			schema_json = json.loads(f.read())
+
+			for field in schema_json['dataset_fields']:
+				if field["required"] == "true" and not package[field["field_name"]]:
+					valid = False
+					break
+
+			for resource_field in schema_json['resource_fields']:
+				for resource in package["resources"]:
+					if resource_field["required"] == "true" and not resource[resource_field["field_name"]]:
+						valid = False
+						break
+
+		except ValueError as e:
+			log.info('invalid json: %s' % e)
+
+	return valid
