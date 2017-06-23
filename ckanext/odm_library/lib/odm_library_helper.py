c09146294a89e5f416bd82c9963bfa82d6349412
Matt Fullerton <matt.fullerton@gmail.com>
2017-06-23 18:31:15 +0200

    remove genshi

--- ckanext/odm_library/lib/odm_library_helper.py
+++ ckanext/odm_library/lib/odm_library_helper.py
@@ -8,7 +8,7 @@ import os
 import logging
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from genshi.template.text import NewTextTemplate
+#TODO from genshi.template.text import NewTextTemplate
 from ckan.lib.base import render
 
 DEBUG = True
@@ -30,7 +30,7 @@ def create_default_issue_library_record(pkg_info):
       't0': toolkit._("Thank you for uploading this item. Instructions about vetting system available on https://wiki.opendevelopmentmekong.net/partners:content_review#instructions_for_default_issue_on_library_records")
     }
 
-    issue_message = render('messages/default_issue_library_record.txt',extra_vars=extra_vars,loader_class=NewTextTemplate)
+    issue_message = "TODO" #render('messages/default_issue_library_record.txt',extra_vars=extra_vars,loader_class=NewTextTemplate)
 
     params = {'title':'User Library record Upload Checklist','description':issue_message,'dataset_id':pkg_info['id']}
     toolkit.get_action('issue_create')(data_dict=params)
